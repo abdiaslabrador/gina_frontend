aecddd12f14eafe53d4b3fad3abdeda7c5c71492
Abdias labrador <abdiaslabrador@gmail.com>
2023-01-20 00:46:36 -0400

    se hizo solamente el diseño de los dientes en css falta los state y que se conecte con la base de datos

--- src/components/odontology/odontograma/Odontograma.tsx
+++ src/components/odontology/odontograma/Odontograma.tsx
@@ -8,8 +8,138 @@ const Odontograma = () => {
   const router = useRouter();
 
   return (
-    <div className={ondontoCss["odontograma"]}>
-          desde odonto
+    <div className={ondontoCss["container"]}>
+      <div className={ondontoCss["odont"]}>
+      <div className={ondontoCss["odont__frame"]}>
+         <div className={ondontoCss["odont__dash"]}>
+            <div>E</div>
+            <div>M</div>
+            <div>?</div>
+            <div>X</div>
+            <div>18</div>
+         </div>
+         <div className={ondontoCss["odont__circle"]}>
+            <div className={ondontoCss["odont__line"]}>
+            </div>
+            <div className={ondontoCss["odont__ring"]}>
+              <div className={`${ondontoCss["odont__molar_parts"]} ${ondontoCss["tooth"]}`}>
+                <div className={ondontoCss["one"]}>
+                  	<div className={`${ondontoCss["point"]} ${ondontoCss["point--red_blue"]}`}>
+                  &bull;
+                    </div>
+                </div>
+                <div className={ondontoCss["two"]}></div>
+                <div className={ondontoCss["three"]}></div>
+                <div className={ondontoCss["four"]}></div>
+                <div className={ondontoCss["five"]}></div>
+              </div>
+            </div>
+         </div>
+      </div>
+      <div className={ondontoCss["odont__frame"]}>
+         <div className={ondontoCss["odont__dash"]}>
+            <div>E</div>
+            <div>M</div>
+            <div>?</div>
+            <div>X</div>
+            <div>18</div>
+         </div>
+         <div className={ondontoCss["odont__circle"]}>
+            <div className={ondontoCss["odont__line"]}>
+            </div>
+            <div className={ondontoCss["odont__ring"]}>
+              <div className={`${ondontoCss["odont__molar_parts"]} ${ondontoCss["tooth"]}`}>
+                <div className={ondontoCss["one"]}>
+                  	<div className={`${ondontoCss["point"]} ${ondontoCss["point--red"]}`}>
+                  &bull;
+                    </div>
+                </div>
+                <div className={ondontoCss["two"]}></div>
+                <div className={ondontoCss["three"]}></div>
+                <div className={ondontoCss["four"]}></div>
+                <div className={ondontoCss["five"]}></div>
+              </div>
+            </div>
+         </div>
+      </div>
+      <div className={ondontoCss["odont__frame"]}>
+         <div className={ondontoCss["odont__dash"]}>
+            <div>E</div>
+            <div>M</div>
+            <div>?</div>
+            <div>X</div>
+            <div>18</div>
+         </div>
+         <div className={ondontoCss["odont__circle"]}>
+            <div className={ondontoCss["odont__line"]}>
+            </div>
+            <div className={ondontoCss["odont__ring"]}>
+              <div className={`${ondontoCss["odont__molar_parts"]} ${ondontoCss["tooth"]}`}>
+                <div className={ondontoCss["one"]}>
+                  	<div className={`${ondontoCss["point"]} ${ondontoCss["point--blue"]}`}>
+                  &bull;
+                    </div>
+                </div>
+                <div className={ondontoCss["two"]}></div>
+                <div className={ondontoCss["three"]}></div>
+                <div className={ondontoCss["four"]}></div>
+                <div className={ondontoCss["five"]}></div>
+              </div>
+            </div>
+         </div>
+      </div>
+      <div className={ondontoCss["odont__frame"]}>
+         <div className={ondontoCss["odont__dash"]}>
+            <div>E</div>
+            <div>M</div>
+            <div>?</div>
+            <div>X</div>
+            <div>18</div>
+         </div>
+         <div className={ondontoCss["odont__circle"]}>
+            <div className={ondontoCss["odont__line"]}>
+            </div>
+            <div className={ondontoCss["odont__ring"]}>
+              <div className={`${ondontoCss["odont__molar_parts"]} ${ondontoCss["tooth"]}`}>
+                <div className={ondontoCss["one"]}>
+                  	<div className={`${ondontoCss["point"]} ${ondontoCss["point--red_blue"]}`}>
+                  &bull;
+                    </div>
+                </div>
+                <div className={ondontoCss["two"]}></div>
+                <div className={ondontoCss["three"]}></div>
+                <div className={ondontoCss["four"]}></div>
+                <div className={ondontoCss["five"]}></div>
+              </div>
+            </div>
+         </div>
+      </div>
+      <div className={ondontoCss["odont__frame"]}>
+         <div className={ondontoCss["odont__dash"]}>
+            <div>E</div>
+            <div>M</div>
+            <div>?</div>
+            <div>X</div>
+            <div>18</div>
+         </div>
+         <div className={ondontoCss["odont__circle"]}>
+            <div className={ondontoCss["odont__line"]}>
+            </div>
+            <div className={ondontoCss["odont__ring"]}>
+              <div className={`${ondontoCss["odont__tooth_parts"]} ${ondontoCss["tooth"]}`}>
+                <div className={ondontoCss["one"]}>
+                  	<div className={`${ondontoCss["point"]} ${ondontoCss["point--red_blue"]}`}>
+                  &bull;
+                    </div>
+                </div>
+                <div className={ondontoCss["two"]}></div>
+                <div className={ondontoCss["three"]}></div>
+                <div className={ondontoCss["four"]}></div>
+              </div>
+            </div>
+         </div>
+      </div>
+    </div>
     </div>
   );
 };
